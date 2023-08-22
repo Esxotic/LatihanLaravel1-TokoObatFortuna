357769e318efd3b5b62136e36a45572ec0c55caf
Esxotic <wayanjerman63@gmail.com>
2023-08-22 18:16:31 +0800

    initial commit

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,57 @@
+<!-- Sidebar -->
+<ul
+class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
+id="accordionSidebar"
+>
+    <!-- Sidebar - Brand -->
+    <a
+    class="sidebar-brand d-flex align-items-center justify-content-center"
+    href="index.html"
+    >
+    <div class="sidebar-brand-icon">
+        <i
+        ><img src="img/Untitled2.ico" alt="" class="img-circle" id="logo"
+        /></i>
+    </div>
+    <div class="sidebar-brand-text mx-3">Fortuna</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider" />
+
+    <!-- Heading -->
+    <div class="sidebar-heading">Menu</div>
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+    <a class="nav-link" href="index.html">
+        <i class="fa fa-tachometer" aria-hidden="true"></i>
+        <span>Dashboard</span></a
+    >
+    </li>
+
+    <!-- Nav Item - Charts -->
+    <li class="nav-item">
+    <a class="nav-link" href="DaftarObat.html">
+        <i class="fa fa-plus-square" aria-hidden="true"></i>
+        <span>Daftar Obat</span></a
+    >
+    </li>
+
+    <!-- Nav Item - Tables -->
+    <li class="nav-item">
+    <a class="nav-link" href="Transaksi.html">
+        <i class="fa fa-exchange" aria-hidden="true"></i>
+        <span>Transaksi</span></a
+    >
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block" />
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+</ul>
+<!-- End of Sidebar -->
---
+++ resources/views/template/main.blade.php
@@ -0,0 +1,93 @@
+@include('partials.head')
+
+  <body id="page-top">
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+      @include('partials.sidebar')
+
+      <!-- Content Wrapper -->
+      <div id="content-wrapper" class="d-flex flex-column">
+        <!-- Main Content -->
+        <div id="content">
+          @include('partials.topbar')
+
+          <!-- Begin Page Content -->
+          <div class="container-fluid">
+            <!-- Page Heading -->
+            <div
+              class="d-sm-flex align-items-center justify-content-between mb-4"
+            >
+              <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+            </div>
+
+            <!-- Content Row -->
+            <div class="row">
+                @yield('container')
+            </div>
+        </div>
+        <!-- /.container-fluid -->
+        </div>
+        <!-- End of Main Content -->
+
+        @include('partials.footer')
+
+      </div>
+      <!-- End of Content Wrapper -->
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+      <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div
+      class="modal fade"
+      id="logoutModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+            <button
+              class="close"
+              type="button"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            Select "Logout" below if you are ready to end your current session.
+          </div>
+          <div class="modal-footer">
+            <button
+              class="btn btn-secondary"
+              type="button"
+              data-dismiss="modal"
+            >
+              Cancel
+            </button>
+            <a class="btn btn-primary" href="login.html">Logout</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="jquery/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="jquery/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+  </body>
+</html>
